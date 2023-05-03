e60a21f42e717853d4efeb2cfc3152c473e474b9
Amar Kumar <84828275+Amar985@users.noreply.github.com>
2023-05-03 22:39:01 +0530

    Update RegistrationForm.java

--- RegistrationForm.java
+++ RegistrationForm.java
@@ -4,6 +4,9 @@ import javax.swing.*;
 public class Register {
     public static void main(String[] args){
 
+        JFrame frame=new JFrame();
+        frame.setTitle("Student Register Form");
+        frame.setVisible(true);
 
 
     }
