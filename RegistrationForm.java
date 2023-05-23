c2ea71e3419c72c36862177dfbe90a149d8ba816
Amar Kumar <84828275+Amar985@users.noreply.github.com>
2023-05-23 21:47:43 +0530

    Update RegistrationForm.java

--- RegistrationForm.java
+++ RegistrationForm.java
@@ -1,36 +1,4 @@
-import java.awt.*;
-import javax.swing.*;
- 
 
-public class Register {
-    public static void main(String[] args){
-        JFrame frame=new JFrame();
-        frame.setTitle("Student Register Form");
-        frame.setVisible(true);
-        frame.setBounds(500,100,500,700);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setResizable(true);
-
-        JLabel message=new JLabel("Register a new Student");
-        message.setFont(new Font("Courier", Font.BOLD, 20));
-        frame.add(message);
-
-        //Amar Kumar
-        JLabel nameLabel=new JLabel("Name");
-        JTextField nameField=new JTextField();
-        frame.add(nameLabel);
-        frame.add(nameField);
-
-        JLabel genderLabel= new JLabel("Gender");
-        JRadioButton genderMale= new JRadioButton("Male", true);
-        JRadioButton genderFemale= new JRadioButton("Female");
-
-        frame.add(genderLabel);
-        frame.add(genderMale);
-        frame.add(genderFemale);
-        ButtonGroup genderGroup= new ButtonGroup();
-        genderGroup.add(genderMale);
-        genderGroup.add(genderFemale);
 
         JLabel mobileNOLabel=new JLabel("Mobile No");
         JTextField mobileNoField=new JTextField();
